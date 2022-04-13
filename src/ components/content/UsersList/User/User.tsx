aa2d370730efd854b4e415fc9899ebe4ece8fee6
diegobusr <diego.ebr94@gmail.com>
2022-04-12 20:43:54 -0700

    added new components for the Search feature

---
+++ src/ components/content/UsersList/User/User.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+  return (
+    <>
+      <h3> I'm a user </h3>
+    </>
+  );
+};
+
+export default User;
---
+++ src/ components/content/UsersList/UsersList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UsersList = () => {
+  return (
+    <>
+      <h3> Users List </h3>
+    </>
+  );
+};
+
+export default UsersList;
